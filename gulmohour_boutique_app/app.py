e6b7821335f84b2bad36a65019bb058e057d0532
Sarika Gupta <sarika0607@gmail.com>
2025-11-02 23:18:42 +0530

    Made changes to firebase

--- gulmohour_boutique_app/app.py
+++ gulmohour_boutique_app/app.py
@@ -42,9 +42,11 @@ WHATSAPP_CONFIG = {
 
 # 1. Initialize Firebase App
 try:
-    load_dotenv()
+    # load_dotenv()
 
-    cred = credentials.Certificate(os.getenv("FIREBASE_KEY_PATH"))
+    firebase_key_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+    cred = credentials.Certificate(firebase_key_path)
 
     firebase_admin.initialize_app(cred, {
     'storageBucket': 'gulmohour-boutique.firebasestorage.app'
